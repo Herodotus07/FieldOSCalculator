ed8ed8786786288271a26d22295985ad55a86597
Herodotus07 <kgilbertson@MacBook-Air.local>
2017-03-05 13:09:43 -0600

    Push it!

---
+++ Field Estimator 2.0/Field Estimator 2.0/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Field Estimator 2.0
+//
+//  Created by Karl Gilbertson on 2/10/17.
+//  Copyright © 2017 Karl Gilbertson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+       
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Field Estimator 2.0/Field Estimator 2.0Tests/Field_Estimator_2_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Field_Estimator_2_0Tests.swift
+//  Field Estimator 2.0Tests
+//
+//  Created by Karl Gilbertson on 2/10/17.
+//  Copyright © 2017 Karl Gilbertson. All rights reserved.
+//
+
+import XCTest
+@testable import Field_Estimator_2_0
+
+class Field_Estimator_2_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
